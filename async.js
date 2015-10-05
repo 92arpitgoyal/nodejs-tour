0ae575d50e2bf6fa07ca1d01266fcabbcc98267e
92arpitgoyal@github.com <92arpit.goyal@gmail.com>
2015-10-05 17:24:53 +0530

    removed variable typos

--- async.js
+++ async.js
@@ -4,8 +4,8 @@ var fileSystem = require('fs');
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     var bookReaing = fileSystem.createReadStream('someHoarding.txt');
-    reaing.on('data', writeCallback);
-    reaing.on('close', closeCallback);
+    bookReaing.on('data', writeCallback);
+    bookReaing.on('close', closeCallback);
     function writeCallback(data){
         response.write(data);
     }
