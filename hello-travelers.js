12a9dac49d1be9290769438d0ae1f4b76dde22de
92arpitgoyal@github.com <92arpit.goyal@gmail.com>
2015-10-05 17:21:32 +0530

    variable typo

--- hello-travelers.js
+++ hello-travelers.js
@@ -1,8 +1,8 @@
 var http = require('http');
 
 http.createServer(function (request, response) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Travelers\n');
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello Travelers\n');
 }).listen(8080);
 
 console.log('Server running at http://127.0.0.1:8080/');
